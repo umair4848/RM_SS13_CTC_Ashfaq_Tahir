b92d73453350502343124fd880936dcc0172a2d4
jenkins <jenkins@BRICS-HW-Server.(none)>
2012-08-16 18:01:04 +0200

    update jenkins script

--- other/jenkins/brsu_jenkins.sh
+++ other/jenkins/brsu_jenkins.sh
@@ -14,22 +14,7 @@ echo "-------------------------------------------------------"
 echo ""
 
 
-write_rosinstall(){
-	STACK="$1"
-	echo "- git: 
-    local-name: $STACK
-    uri: git@github.com:---GITHUBUSER---/$STACK.git
-    branch-name: master" >> $WORKSPACE/../$REPOSITORY.rosinstall
-}
 
-
-check_stack(){
-	STACK="$1"
-	user=`git config --global github.user`
-	token=`git config --global github.token`
-	wget --post-data "login=$user&token=$token" --spider https://github.com/"$GITHUBUSER"/"$STACK"/blob/master/Makefile --no-check-certificate 2> $WORKSPACE/../wget_response.txt
-	return $(grep -c "200 OK" $WORKSPACE/../wget_response.txt)
-}
 
 do_testing(){
 	# sleep to finish running tests
@@ -58,65 +43,22 @@ do_testing(){
 
 # installing ROS release
 sudo apt-get autoclean
-#sudo apt-get update
-#sudo apt-get install python-setuptools -y
-#sudo easy_install -U rosinstall
-#sudo apt-get install ros-$RELEASE-care-o-bot -y
-
-# create .rosinstall file
-#echo "- other: {local-name: /opt/ros/---ROSRELEASE---/ros}
-#- other: {local-name: /opt/ros/---ROSRELEASE---/stacks}
-#" > $WORKSPACE/../$REPOSITORY.rosinstall
-
-# get dependencies
-#rm $WORKSPACE/../$REPOSITORY.deps
-#wget https://github.com/ipa320/hudson/raw/master/run/"$REPOSITORY".deps -O $WORKSPACE/../$REPOSITORY.deps --no-check-certificate
-
-#echo ""
-#echo "--------------------------------------------------------------------------------"
-#echo "Checking dependencies for $REPOSITORY"
-#while read myline
-#do
-#  # check if stack is forked > true: include into .rosinstall file / false: check if it's reasonable to continue
-#  echo "Performing check on stack: $myline"
-#  check_stack $myline
-#  if [ $? != 0 ]; then
-#    write_rosinstall $myline
-#    echo "  INFO: Using stack $myline from $GITHUBUSER at github.com"
-#  else
-#    # repository is for sure just dependent on stack > continue 
-#    echo "  WARNING: Stack $myline not forked to $GITHUBUSER at github.com. Using release stack instead."
-#  fi
-#done < $WORKSPACE/../$REPOSITORY.deps
-#echo "--------------------------------------------------------------------------------"
-#echo ""
-
-# delete unnecessary wget_response.txt
-#rm $WORKSPACE/../wget_response.txt
-
-# generate .rosinstall file
-#sed -i "s/---GITHUBUSER---/$GITHUBUSER/g" $WORKSPACE/../$REPOSITORY.rosinstall
-#sed -i "s/---ROSRELEASE---/$RELEASE/g" $WORKSPACE/../$REPOSITORY.rosinstall
-#sed -i "s/---JOBNAME---/$JOB_NAME/g" $WORKSPACE/../$REPOSITORY.rosinstall
-#sed -i "s/---REPOSITORY---/$REPOSITORY/g" $WORKSPACE/../$REPOSITORY.rosinstall
-
-# perform clean rosinstall
-#rm $WORKSPACE/../.rosinstall
-#rosinstall $WORKSPACE/../ $WORKSPACE/../$REPOSITORY.rosinstall $WORKSPACE --delete-changed-uris
+sudo apt-get update
+sudo apt-get install python-setuptools -y
+sudo easy_install -U rosinstall vcstools
 
 # setup ROS environment
 . /opt/ros/$RELEASE/setup.bash
+
+rosinstall $WORKSPACE/../ext_pkgs $WORKSPACE/repository.rosinstall --delete-changed-uris --rosdep-yes
 
 # define amount of ros prozesses during build for multi-prozessor machines
 COUNT=$(cat /proc/cpuinfo | grep 'processor' | wc -l)
 COUNT=$(echo "$COUNT*2" | bc)
 export ROS_PARALLEL_JOBS=-j$COUNT
 
-#build farm stuff
-#export PATH=/usr/lib/ccache/:$PATH
-#export DISTCC_HOSTS='localhost distcc@hektor.ipa.fhg.de/8 distcc@chaos.ipa.fhg.de/4'
-#export CCACHE_PREFIX=distcc
-#export ROS_PARALLEL_JOBS=-j28
+
+export ROS_PACKAGE_PATH=$WORKSPACE/..:$ROS_PACKAGE_PATH
 
 echo ""
 echo "-------------------------------------------------------"
@@ -128,13 +70,9 @@ echo "-------------------------------------------------------"
 echo ""
 
 # installing dependencies and building
-#rosdep install $REPOSITORY -y
-export ROS_PACKAGE_PATH=$WORKSPACE:$ROS_PACKAGE_PATH
-echo $ROS_PACKAGE_PATH
 cd $WORKSPACE
-pwd
-ls 
-rosmake * --skip-blacklist --profile
+rosdep install * -y
+rosmake -r * --skip-blacklist --profile
 
 # check if building is succesfull, otherwise don't perform test and exit
 if [ $? != "0" ]; then
