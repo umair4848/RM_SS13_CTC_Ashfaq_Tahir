a8dcb8ddf62b5b8a8ac2b056ade8fd246a974f1b
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-09-05 09:01:59 +0200

    add autoremove and install of all ros-stacks to jenkins script

--- other/jenkins/brsu_jenkins.sh
+++ other/jenkins/brsu_jenkins.sh
@@ -19,6 +19,8 @@ sudo apt-get autoclean
 sudo apt-get update
 sudo apt-get install python-setuptools -y
 sudo easy_install -U rosinstall vcstools
+sudo apt-get install ros-electric-*
+sudo apt-get autoremove
 
 # setup ROS environment
 . /opt/ros/$RELEASE/setup.bash
