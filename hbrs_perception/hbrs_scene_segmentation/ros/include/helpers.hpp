3ccec64b210358c34d3f79844b8925e65090453c
Sergey Alexandrov <alexandrov88@gmail.com>
2012-09-12 18:32:57 +0200

    Add a function to compute the area of a planar polygon.

--- hbrs_perception/hbrs_scene_segmentation/ros/include/helpers.hpp
+++ hbrs_perception/hbrs_scene_segmentation/ros/include/helpers.hpp
@@ -38,5 +38,29 @@ void convertPlanarPolygon(const hbrs_msgs::PlanarPolygon& ros_polygon, PlanarPol
   pcl_polygon.setCoefficients(coefficients);
 }
 
+double computePlanarPolygonArea(const PlanarPolygon& polygon)
+{
+  const auto& normal = polygon.getCoefficients();
+  const auto& points = polygon.getContour();
+
+  // Find axis with largest normal component and project onto perpendicular plane
+  int k0, k1, k2;
+  k0 = (std::fabs(normal[0]) > std::fabs(normal[1])) ? 0 : 1;
+  k0 = (std::fabs(normal[k0]) > std::fabs(normal[2])) ? k0 : 2;
+  k1 = (k0 + 1) % 3;
+  k2 = (k0 + 2) % 3;
+
+  double area = 0;
+  for (size_t i = 0; i < points.size(); i++)
+  {
+    size_t j = (i + 1) % points.size();
+    float p1[3] = { points[i].x, points[i].y, points[i].z };
+    float p2[3] = { points[j].x, points[j].y, points[j].z };
+    area += p1[k1] * p2[k2] - p1[k2] * p2[k1];
+  }
+
+  return std::fabs(area) / (2 * std::fabs(normal[k0]));
+}
+
 #endif /* HELPERS_HPP */
 
