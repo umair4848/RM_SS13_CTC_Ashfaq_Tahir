4da9bd79a60864f60513de597596c790b595791d
Sergey Alexandrov <alexandrov88@gmail.com>
2012-09-12 13:44:28 +0200

    Add PlanarPolygonVisualizer to the new visualization package.

---
+++ hbrs_perception/hbrs_visualization/ros/include/color.h
@@ -0,0 +1,39 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+namespace hbrs
+{
+
+namespace visualization
+{
+
+struct Color
+{
+
+  Color(float nr, float ng, float nb) : r(nr), g(ng), b(nb) { }
+
+  float r;
+  float g;
+  float b;
+
+  enum Name
+  {
+    SALMON,
+  };
+
+  Color(Name name)
+  {
+    switch (name)
+    {
+      SALMON: r = 0.98, g = 0.50, b = 0.45; break;
+    }
+  }
+
+};
+
+}
+
+}
+
+#endif /* COLOR_H */
+
---
+++ hbrs_perception/hbrs_visualization/ros/include/planar_polygon_visualizer.h
@@ -0,0 +1,62 @@
+#ifndef PLANAR_POLYGON_VISUALIZER_H
+#define PLANAR_POLYGON_VISUALIZER_H
+
+#include <string>
+
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/geometry/planar_polygon.h>
+
+#include "color.h"
+
+namespace hbrs
+{
+
+namespace visualization
+{
+
+class PlanarPolygonVisualizer
+{
+
+public:
+
+  PlanarPolygonVisualizer(const std::string& topic_name,
+                          const std::string& frame_id,
+                          const Color& color,
+                          bool check_subscribers = true);
+
+  PlanarPolygonVisualizer(const std::string& topic_name,
+                          const std::string& frame_id,
+                          Color::Name color,
+                          bool check_subscribers = true);
+
+  template<typename PointT>
+  void publish(const pcl::PlanarPolygon<PointT>& polygon);
+
+  /** Fill the fields of the marker object so that it visualizes the provided
+    * vector of points by drawing a polyline through them. */
+  template<typename PointT>
+  void buildPolygonMarker(const typename pcl::PointCloud<PointT>::VectorType& points,
+                          visualization_msgs::Marker& marker,
+                          int id = 1);
+
+private:
+
+  ros::Publisher marker_publisher_;
+
+  const std::string frame_id_;
+  const Color color_;
+  bool check_subscribers_;
+
+};
+
+}
+
+}
+
+#include "impl/planar_polygon_visualizer.hpp"
+
+#endif /* PLANAR_POLYGON_VISUALIZER_H */
+
