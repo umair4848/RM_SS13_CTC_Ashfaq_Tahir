71a3c71e86832e7cc066f5d4ad72e2718334c435
Sergey Alexandrov <alexandrov88@gmail.com>
2012-09-15 20:06:54 +0200

    Add more colors and const modifier to cast operators in Color.

--- hbrs_perception/hbrs_visualization/ros/include/color.h
+++ hbrs_perception/hbrs_visualization/ros/include/color.h
@@ -26,6 +26,14 @@ struct Color
     SALMON,
     TEAL,
     DEEP_PINK,
+    SANGRIA,
+    SEA_BLUE,
+    SEA_GREEN,
+    SCARLET,
+    PUMPKIN,
+    JASMINE,
+    IVORY,
+    GAINSBORO,
   };
 
   Color(Name name)
@@ -35,17 +43,25 @@ struct Color
       case SALMON:        r = 0xFA, g = 0x80, b = 0x72; break;
       case TEAL:          r = 0x00, g = 0x80, b = 0x80; break;
       case DEEP_PINK:     r = 0xFF, g = 0x14, b = 0x93; break;
+      case SANGRIA:       r = 0x92, g = 0x00, b = 0x0A; break;
+      case SEA_BLUE:      r = 0x00, g = 0x69, b = 0x94; break;
+      case SEA_GREEN:     r = 0x2E, g = 0x8B, b = 0x57; break;
+      case SCARLET:       r = 0xFF, g = 0x24, b = 0x00; break;
+      case PUMPKIN:       r = 0xFF, g = 0x75, b = 0x18; break;
+      case JASMINE:       r = 0xF8, g = 0xDE, b = 0x7E; break;
+      case IVORY:         r = 0xFF, g = 0xFF, b = 0xF0; break;
+      case GAINSBORO:     r = 0xDC, g = 0xDC, b = 0xDC; break;
       default:            r = 0xFF, g = 0xFF, b = 0xFF; break;
     }
   }
 
-  operator float()
+  operator float() const
   {
     pcl::PointXYZRGB point(r, g, b);
     return point.rgb;
   }
 
-  operator std_msgs::ColorRGBA()
+  operator std_msgs::ColorRGBA() const
   {
     std_msgs::ColorRGBA color;
     color.r = r / 255.0;
