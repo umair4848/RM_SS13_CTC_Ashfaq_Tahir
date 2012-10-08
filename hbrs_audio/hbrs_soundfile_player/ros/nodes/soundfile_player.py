bc2925f31038e4bedddad3f9c3ebc707668dad2e
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-10-08 23:34:50 +0100

    add sound play package

---
+++ hbrs_audio/hbrs_soundfile_player/ros/nodes/soundfile_player.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+PACKAGE = 'hbrs_soundfile_player'
+SERVICE = 'play_soundfile'
+
+import roslib
+roslib.load_manifest(PACKAGE)
+
+import rospy
+import hbrs_srvs.srv
+import commands
+import os
+
+
+def play_soundfile_cb(request):
+    rospy.loginfo('Received [%s] request.' % SERVICE)
+        
+    wav_path = commands.getoutput("rospack find raw_script_server")
+    filename = wav_path + "/common/files/" + request.str
+
+    rospy.loginfo("Playing <<%s>>", filename)
+    os.system("aplay -q " + filename)
+    
+    empty_response = hbrs_srvs.srv.PassStringResponse()
+    
+    return empty_response
+
+
+if __name__ == '__main__':
+    rospy.init_node("hbrs_soundfile_player")
+    
+    srv_play_soundfile = rospy.Service(SERVICE, hbrs_srvs.srv.PassString, play_soundfile_cb)
+    rospy.loginfo('Advertising service: [%s]' % SERVICE)
+    rospy.spin()
