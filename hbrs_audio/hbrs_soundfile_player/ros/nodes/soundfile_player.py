dacc09efea9abf7dd3b398626b3f646c7987ed2a
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-11-15 15:17:15 +0100

    advertise service under the nodes name instead of a namespace

--- hbrs_audio/hbrs_soundfile_player/ros/nodes/soundfile_player.py
+++ hbrs_audio/hbrs_soundfile_player/ros/nodes/soundfile_player.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 PACKAGE = 'hbrs_soundfile_player'
-SERVICE = 'play_soundfile'
+SERVICE = '~play_soundfile'
 
 import roslib
 roslib.load_manifest(PACKAGE)
@@ -18,10 +18,14 @@ def play_soundfile_cb(request):
     wav_path = commands.getoutput("rospack find raw_script_server")
     filename = wav_path + "/common/files/" + request.str
 
+    empty_response = hbrs_srvs.srv.PassStringResponse()
+
+    if not os.path.exists(filename):
+        rospy.logerr("file \"%s\" does not exist", filename)
+        return empty_response
+        
     rospy.loginfo("Playing <<%s>>", filename)
     os.system("aplay -q " + filename)
-    
-    empty_response = hbrs_srvs.srv.PassStringResponse()
     
     return empty_response
 
@@ -30,5 +34,6 @@ if __name__ == '__main__':
     rospy.init_node("hbrs_soundfile_player")
     
     srv_play_soundfile = rospy.Service(SERVICE, hbrs_srvs.srv.PassString, play_soundfile_cb)
-    rospy.loginfo('Advertising service: [%s]' % SERVICE)
+    rospy.loginfo('Advertising service: %s' % SERVICE)
+    rospy.loginfo('Node successfully initialized')
     rospy.spin()
