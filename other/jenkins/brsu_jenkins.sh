1b961a2547169007e3b1d82d96546dd9f0d4fcc7
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-12-19 22:01:37 +0100

    add debug outputs

--- other/jenkins/brsu_jenkins.sh
+++ other/jenkins/brsu_jenkins.sh
@@ -15,21 +15,40 @@ echo ""
 
 
 # installing ROS release
+echo "\n apt autoclean, update, upgrade and dist-upgrade"
+echo "##################################################\n"
 sudo apt-get autoclean
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
+
+echo "\n install python-setuptools, rosinstall and vcstools"
+echo "##################################################\n"
 sudo apt-get install python-setuptools -y
 sudo easy_install -U rosinstall vcstools
+
+echo "\n install python-setuptools, rosinstall and vcstools"
+echo "##################################################\n"
 $WORKSPACE/repository.debs -y
+
+echo "\n ros-$RELEASE-*"
+echo "##################################################\n"
 sudo apt-get install ros-$RELEASE-* -y
+
+echo "\n remove moveit packages in fuerte"
+echo "##################################################\n"
 sudo apt-get remove ros-fuerte-moveit-core  ros-fuerte-moveit-msgs -y
+
+echo "\n autoremove"
+echo "##################################################\n"
 sudo apt-get autoremove -y
 
 # setup ROS environment
 . /opt/ros/$RELEASE/setup.bash
 
 # execute repository.rosinstall of each repository
+echo "\n rosinstall"
+echo "##################################################\n"
 rm -rf $WORKSPACE/../ext_pkgs/.rosinstall
 rosinstall $WORKSPACE/../ext_pkgs /opt/ros/$RELEASE $WORKSPACE/repository.rosinstall --delete-changed-uris --rosdep-yes
 
