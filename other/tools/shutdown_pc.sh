e8948a9d5562d797615a52479ad4c002a231a6e4
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2013-01-14 16:22:53 +0100

    move to separate setup-and-tools repository

--- other/tools/shutdown_pc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Param1: pc to shutdown
-# Param2: -r or -h
-# need to execute "sudo chmod +s /sbin/shutdown" once on the machines which you want shutdown remotely
- 
-ssh $1 "shutdown" $2 "now" &
